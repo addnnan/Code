ba7d159db487aebea7e147286cfabc9360d6e53b
adnnaan <shahadnanx@gmail.com>
2022-12-30 20:54:49 +0530

    Uploading a file

---
+++ Python/fibonacci.py
@@ -0,0 +1,13 @@
+num = int(input("Enter:"))
+
+
+def fibonacci(n):
+    if n == 1:
+        return (0)
+    elif n <=2:
+        return 1
+    else:
+        return (fibonacci(n - 1) + fibonacci(n - 2))
+
+for i in range(num):
+    print(fibonacci(i+1))
